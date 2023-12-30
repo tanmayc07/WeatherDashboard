70c0d29e34444e34e783beda10266403c3c48bb5
Tanmay Chaudhari <hctanmay@gmail.com>
2023-12-29 23:10:52 -0500

    :ambulance: Update settings.py to properly set the host

--- WeatherDashboard/settings.py
+++ WeatherDashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://dj-weather-dashboard-85688c46ad2c.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['dj-weather-dashboard-85688c46ad2c.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
